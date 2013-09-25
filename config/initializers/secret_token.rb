82ea54b92c773b298ce4d2f951c3dfdf0e021a34
orangedrum <jean.kaluza@orangedrum.com>
2013-09-25 18:59:14 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrangeBlog::Application.config.secret_key_base = '0f8b3b252c73ea15f390ad9be6367587e399c93f6e8dfd1d3ee76733570dd5447421d075aee4f32771e4ccdbb3f414a0e0eeffd97e3484c03bf12059351557af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OrangeBlog::Application.config.session_store :cookie_store, key: '_orange_blog_session'
